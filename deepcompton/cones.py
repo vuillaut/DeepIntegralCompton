df171914bd44d7ad8148f852cf2e5e11e79df1b9
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2021-12-07 00:12:33 +0100

    make_cone, make_cone_density functions, editing cone_center_distribution.py

--- deepcompton/cones.py
+++ deepcompton/cones.py
@@ -1,10 +1,89 @@
-def make_cone(x, z_isgri, z_picsit):
-    """Create cone data [theta_d, phi_d, delta]
-    """
-    pass
+import numpy as np
 
-def make_cone_densities(source_theta, source_phi, n=100):
-    """Create cone density data for a given source theta and phi value. Use n cones
+import deepcompton.utils as compton
+from deepcompton.progress import printProgressBar
+def make_cone(x, z_isgri, z_picsit, Ee=511):
+    """Single cone
     """
-    pass
+    x1cur = z_isgri
+    x2cur = z_picsit
 
+    energ1cur = x[0]
+    energ2cur = x[1]
+
+    y1cur = x[7]
+    z1cur = -x[6]
+    
+    y2cur = x[9]
+    z2cur = -x[8]
+    
+
+    E0 = energ1cur + energ2cur
+    
+    Ec = E0/(1+2*E0/Ee)
+    
+    # why this condition ??
+    if (energ2cur >= Ec) and (energ1cur <= E0 - Ec):
+        E2 = E0 - energ1cur
+        cotheta = compton.cottheta(energ1cur,E0 - energ1cur)
+
+        A = np.array([x1cur,y1cur,z1cur])
+        B = np.array([x2cur,y2cur,z2cur])
+    
+        theta = compton.colatitudeaxe(B,A)
+        phi = compton.longitudeaxe(B,A)
+        
+        return np.array([theta, phi, cotheta])
+    return None
+
+
+def make_cone_density(theta_source, phi_source, z_isgri, z_picsit, precision=5000., density_precision=2., r=1e14, max_cones=2000000, lon_max=360., lat_max=90.):
+    name = "./save_Compton/theta_"+str(theta_source)+"_phi_"+str(phi_source)+".npy"
+    X = np.load(name).astype(np.float64)
+    N = X.shape[0]
+    # if empty data return None
+    if N==0:
+        return None
+    
+    # density grid
+    density = np.zeros((int(lon_max/density_precision),int(lat_max/density_precision)))
+    
+    # cone counter
+    ncones = 0
+    
+    # for each row in the data create a cone
+    progress_msg = "Loading cones, theta:{}, phi:{}".format(theta_source,phi_source)
+    printProgressBar(0, N, prefix = progress_msg, suffix = 'Complete', length = 50)
+    for i in range(N):
+        # while cone count is not reached
+        if ncones < max_cones:
+            cone = make_cone(X[i,:], z_isgri, z_picsit)
+            if cone is not None:
+                [theta,phi,cotheta]=cone
+                x1cur = z_isgri
+                x2cur = z_picsit
+    
+                y1cur = X[i,7]
+                z1cur = -X[i,6]
+                y2cur = X[i,9]
+                z2cur = -X[i,8]
+                
+                colat = compton.colatconer(r, x1cur, y1cur, z1cur, theta, phi, cotheta, precision) 
+                longit = compton.longitconer(r, x1cur, y1cur, z1cur, theta, phi, cotheta, precision)
+                
+                hemisphere = (colat < 90)
+                longit = longit[hemisphere]
+                colat = colat[hemisphere]
+                d = np.zeros((int(lon_max/density_precision), int(lat_max/density_precision)))
+                
+                l = ((abs(longit)%lon_max)/density_precision).astype(int)
+                c = ((abs(colat)%lat_max)/density_precision).astype(int)
+        
+                d[l,c]=1.
+        
+                density += d
+                
+                ncones += 1
+        printProgressBar(i + 1, N, prefix = progress_msg, suffix = 'Complete', length = 50) 
+    return density
+ 
