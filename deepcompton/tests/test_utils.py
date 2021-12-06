c4cf35a983dd70921c8dd0b52a48bbdaa3425d5c
vuillaut <thomas.vuillaume@gmail.com>
2021-12-07 00:42:21 +0100

    fix test utils

--- deepcompton/tests/test_utils.py
+++ deepcompton/tests/test_utils.py
@@ -2,5 +2,10 @@ from deepcompton import utils
 
 
 def test_get_test_data_path():
-    path = utils.get_data_path()
+    path = utils.get_test_data_path()
     assert path.endswith('theta_42_phi_104.npy')
+
+def test_load_data():
+    path = utils.get_test_data_path()
+    data = utils.load_data(path)
+    assert len(data) > 0
