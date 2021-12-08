9586e0db443351e30b157eff0ce11551ceb5352b
Carlo Maria Zwölf <carlo-maria.zwolf@obspm.fr>
2021-12-08 17:47:30 +0100

    Adding a layer for solving dimensionality issue
    In the definition of the model (defineNetwork function) the GlobalAveragePooling1D have been added to solve dimensionality issue. As a consequence some reshaping performed in runModelAndReturnPrediction are useless and then removed.

--- deepcompton/TensorFlowWaveNet.py
+++ deepcompton/TensorFlowWaveNet.py
@@ -8,18 +8,16 @@ def defineNetwork():
   for rate in (1, 2, 4, 8) * 2:
     model.add(keras.layers.Conv1D(filters=20, kernel_size=2, padding="causal", activation="relu", dilation_rate=rate))
   model.add(keras.layers.Conv1D(filters=10, kernel_size=1))
-  model.add(keras.layers.Conv1D(filters=2, kernel_size=1))
+  model.add(keras.layers.GlobalAveragePooling1D())
+  model.add(keras.layers.Dense(2))
   return model
 
 def runModelAndReturnPrediction(targetPhi,targetTheta,anglesTrain,anglesTest, epochs=20, sizeofBatch=120):
   Y_train = np.transpose([targetPhi,targetTheta])
   X_train = anglesTrain
-  
-  #build tensor versions for target to fit the dimensionality requirements from the WaveNet ouputs
-  TensYtrain = Y_train[:,np.newaxis,:]
   model = defineNetwork()
   model.compile(loss="mse", optimizer="adam", metrics=["mse"])
-  model.fit(X_train, TensYtrain, epochs=epochs, batch_size=sizeofBatch)
+  model.fit(X_train, Y_train, epochs=epochs, batch_size=sizeofBatch)
   Y_pred_train = model.predict(X_train)
   Y_pred_test = model.predict(anglesTest)
   return Y_pred_train, Y_pred_test, model
