5c676fa51c8724a33f08149e60a2198d58ae3541
Carlo Maria Zwölf <carlo-maria.zwolf@obspm.fr>
2021-12-08 10:21:25 +0100

    Create TensorFlowSimpleRNN.py
    A simple RNN to make prediction on angular data

---
+++ deepcompton/TensorFlowSimpleRNN.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+def defineNetword():
+  model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 3]),
+    keras.layers.BatchNormalization(),
+    keras.layers.SimpleRNN(20, return_sequences=False),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dense(2)
+])
+  return model
+
+def runModelAndReturnPrediction(targetPhi,targetTheta,anglesTrain,anglesTest, epochs=20, batch_size=120):
+  Y_train = np.array([targetPhi,targetTheta]).reshape(targetPhi.shape[0],-1)
+  X_train = anglesTrain
+  model = defineNetwork()
+  model.compile(loss="mse", optimizer="adam", metrics=["mse"])
+  model.fit(X_train, Y_train, epochs=20, batch_size)
+  Y_pred_train = model.predict(X_train)
+  Y_pred_test = model.predict(anglesTest)
+  return Y_pred_train, Y_pred_test
